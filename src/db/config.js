3619b23e9930751d5d657b5f6790a39f5c0c30f9
CharlyCode0012 <100393106+CharlyCode0012@users.noreply.github.com>
2022-11-02 15:53:56 -0600

    update config.js

--- src/db/config.js
+++ src/db/config.js
@@ -4,7 +4,7 @@ const DB_PORT = process.env.DB_PORT || 3000,
   DB_DATABASE = process.env.DB_DATABASE || 'data_bot',
   DB_USER = process.env.DB_USER || 'root',
   DB_PASSWORD = process.env.DB_PASSWORD || '0906gean',
-  DB_HOST = process.env.INSTANCE_HOST || 'localhost',
+  DB_HOST = process.env.DB_HOST || 'localhost',
   DB_DIALECT = process.env.DB_DIALECT || 'mysql',
   PORT = process.env.PORT || 3000;
 
