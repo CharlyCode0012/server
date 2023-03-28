b66441ff67d029ee2f50b90c6997e020db8b58e4
CharlyCode0012 <charly.code0012@gmail.com>
2023-03-28 02:49:02 -0600

    cors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+  }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
