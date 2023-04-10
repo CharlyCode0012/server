d0c92a63629beb9213df5de519d955dff400f17c
Alejandro David García González <88684972+TheMagicDragonMX@users.noreply.github.com>
2023-04-10 11:59:44 -0600

    Logger for routes

--- src/index.js
+++ src/index.js
@@ -11,11 +11,17 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// TODO: Remove route logger
+app.use((req, res, next) => {
+	console.log('\x1b[33m%s\x1b[0m', `=> ${req.url}`);
+	next();
+});
+
 app.use('/api', apiRouter);
 
 app.listen(process.env.PORT, () => {    
-    console.log("=========================")
-    console.log("Listening on port: " + process.env.PORT);
-    console.log("Server is running! 😎")
-    console.log("=========================\n")
+	console.log("=========================")
+	console.log("Listening on port: " + process.env.PORT);
+	console.log("Server is running! 😎")
+	console.log("=========================\n")
 });
