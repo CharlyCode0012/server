e0f6202ffa2d59325774b60920e957e26159ee37
CharlyCode0012 <charly.code0012@gmail.com>
2023-04-26 00:07:28 -0600

    Catalogs

--- src/routes/api/catalogs.js
+++ src/routes/api/catalogs.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const ExcelJS = require("exceljs")
 
 const {Catalog} = require('../../db/db');
 
@@ -6,6 +7,55 @@ router.get('/', async (req, res)=>{
     const catalogs = await Catalog.findAll();
     res.json(catalogs);
 });
+
+
+
+/**
+ * Returns an xlsx file that contains the info of 
+ * the existing categories in the DB 
+ */
+router.get("/download", async (req, res) => {
+
+    // Get categories from DB
+    const catalogsQuery = await Catalog.findAll()
+    const categories = JSON.parse(JSON.stringify(catalogsQuery)).map(catalog=> ({
+      "id": catalog.id,
+      "name": catalog.name,
+      "description": catalog.description,
+      "state": catalog.state
+    }))
+  
+    // Create excel workbook, where sheets will be stored
+    const workbook = new ExcelJS.Workbook();
+  
+    // Create a sheet and assign to it some columns metadata to insert rows
+    const worksheet = workbook.addWorksheet("Lista de Catalogos")
+    worksheet.columns = [
+      { header: "ID", key: "id", width: 20 },
+      { header: "Nombre", key: "name", width: 25 },
+      { header: "Descripcion", key: "description", width: 50 },
+      { header: "Estado", key: "state", width: 30 },
+    ]
+  
+    // Style each column
+    const [ idColumn, nameColumn, stateColumn ] = [ worksheet.getColumn("id"), worksheet.getColumn("name"), worksheet.getColumn("description"), worksheet.getColumn("state") ];
+    const alignment = { horizontal: "center" };
+    [ idColumn.alignment, nameColumn.alignment, stateColumn.alignment ] = [ alignment, alignment, alignment ];
+  
+    // Style header row
+    const headerRow = worksheet.getRow(1)
+    headerRow.font = { bold: true, size: 14 };
+  
+    // Add data of every category
+    for (const category of categories)
+      worksheet.addRow(category)
+  
+    const fileBuffer = await workbook.xlsx.writeBuffer();
+  
+    res.setHeader('content-disposition', 'attachment; filename="Catalogos.xlsx"');
+    res.setHeader('Access-Control-Expose-Headers', 'content-disposition');
+    res.status(200).end(fileBuffer);
+  });
 
 router.get('/:catalogId', async (req, res)=>{
     const {catalogId} = req.params;
