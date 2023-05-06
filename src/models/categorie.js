e76902342a30691798a422bb41f15dd3330d72b3
CharlyCode0012 <100393106+CharlyCode0012@users.noreply.github.com>
2023-05-06 10:28:24 -0600

    change types to models

---
+++ src/models/categorie.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, type) =>{
+    return sequelize.define('categorie', {
+        id:{
+            type: type.STRING(50),
+            primaryKey: true,
+            autoIncrement: false
+        },
+        name:{
+            type: type.STRING(100),
+            allowNUll: false
+        },
+        state:{
+            type: type.BOOLEAN,
+            allowNUll: false
+        }
+    })
+}
