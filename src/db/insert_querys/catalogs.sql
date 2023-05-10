6e979de599965852142990c95750693abe9af3d8
CharlyCode0012 <100393106+CharlyCode0012@users.noreply.github.com>
2023-05-09 20:03:22 -0600

    sql

--- src/db/insert_querys/catalogs.sql
+++ src/db/insert_querys/catalogs.sql
@@ -4,4 +4,5 @@ VALUES
     ('12', 'Catálogo de calzado', 'Catálogo de calzado para mujeres, hombres y niños', 1, NOW(), NOW()),
     ('13', 'Catálogo de artículos de papelería', 'Catálogo de artículos de papelería y oficina', 1, NOW(), NOW()),
     ('14', 'Catálogo de artículos de limpieza', 'Catálogo de artículos de limpieza y cuidado del hogar', 1, NOW(), NOW()),
-    ('15', 'Catálogo de juguetes', 'Catálogo de juguetes para niños de todas las edades', 1, NOW(), NOW());
+    ('15', 'Catálogo de juguetes', 'Catálogo de juguetes para niños de todas las edades', 1, NOW(), NOW()),
+    ('6', 'Catalogo Electronicos', 'Catálogo de electronicos como computadoras', 1, NOW(), NOW());
