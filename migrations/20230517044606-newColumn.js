0846cb917bfab880f4371487331c8099b4dc3b44
CharlyCode0012 <charly.code0012@gmail.com>
2023-05-16 23:28:52 -0600

    dasdasdasd

--- migrations/20230517044606-newColumn.js
+++ migrations/20230517044606-newColumn.js
@@ -11,6 +11,10 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.addColumn('products', 'description')
+    await queryInterface.addColumn('products', 'description', {
+      type: Sequelize.STRING, 
+      allowNull: true, // Define si el campo permite valores nulos o no
+      defaultValue: '' // Define el valor por defecto del nuevo campo (opcional)
+    });
   }
 };
