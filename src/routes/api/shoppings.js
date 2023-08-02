791c83f8524efa4d3fa23d72a5fcadb18eec3353
CharlyCode0012 <100393106+CharlyCode0012@users.noreply.github.com>
2023-08-02 03:11:45 -0600

    Update shoppings.js

--- src/routes/api/shoppings.js
+++ src/routes/api/shoppings.js
@@ -52,7 +52,7 @@ async function getProductsClient(clientId, timePeriod) {
         },
       ],
       where: optionsWhere,
-      group: ['id_product'],
+      group: ['id_product', 'shopping.date_purchase'], // Incluir shopping.date_purchase en el grupo
       raw: true,
     });
 
